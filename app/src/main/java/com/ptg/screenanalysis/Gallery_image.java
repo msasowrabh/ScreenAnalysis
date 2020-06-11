8ddb56b4e4208798a8c63e80d088762c46f1e445
sowrabh meduri <msasowrabh55@gmail.com>
2020-06-11 13:14:28 +0530

    added google_services.json file for firebase console and added text recognition code in Gallery_image.java file

--- app/src/main/java/com/ptg/screenanalysis/Gallery_image.java
+++ app/src/main/java/com/ptg/screenanalysis/Gallery_image.java
@@ -1,6 +1,7 @@
 package com.ptg.screenanalysis;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.palette.graphics.Palette;
@@ -16,6 +17,14 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
 import java.io.IOException;
 
@@ -64,6 +73,7 @@ public class Gallery_image extends AppCompatActivity {
                 t.setBackgroundColor(light_vib);
                 t=(TextView)findViewById(R.id.muted_color);
                 t.setBackgroundColor(muted);
+                Recognizetext(bitmap);
 
 
 
@@ -98,5 +108,32 @@ public class Gallery_image extends AppCompatActivity {
 
             }*/
         }
+    }
+
+    private void Recognizetext(Bitmap bitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
+                .getCloudTextRecognizer();
+        Task<FirebaseVisionText> result =
+                detector.processImage(image)
+                        .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                                String text=firebaseVisionText.getText();
+                                Toast.makeText(Gallery_image.this,text,Toast.LENGTH_SHORT).show();
+
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        e.printStackTrace();
+                                        Log.e(String.valueOf(e),String.valueOf(e));
+                                        Toast.makeText(Gallery_image.this,String.valueOf(e),Toast.LENGTH_SHORT).show();
+
+                                    }
+                                });
+
     }
 }
